9563cddebbb506ec78958f28bfd6c3413fa7f1be
Kenneth Styppa <kenneth.styppa@web.de>
2023-12-12 17:53:04 +0100

    added nodes to struture abstracts in paragraphs and obtain user input (including obtaining its embedding)

--- kedronlp/src/kedronlp/pipelines/data_processing/pipeline.py
+++ kedronlp/src/kedronlp/pipelines/data_processing/pipeline.py
@@ -1,5 +1,5 @@
 from kedro.pipeline import Pipeline, node, pipeline
-from .nodes import extract_data
+from .nodes import extract_data, process_extract_data, get_user_query
 
 
 def create_pipeline(**kwargs) -> Pipeline:
@@ -10,6 +10,18 @@ def create_pipeline(**kwargs) -> Pipeline:
                 inputs="params:extract_params",
                 outputs="extract_data",
                 name="extract_data_node",
+            ),
+            node(
+                func=process_extract_data,
+                inputs="extract_data",
+                outputs="paragraphed_data",
+                name="process_data_node",
+            ),
+            node(
+                func=get_user_query,
+                inputs=None,
+                outputs="user_query",
+                name="get_user_query_node",
             ),
         ]
     )
